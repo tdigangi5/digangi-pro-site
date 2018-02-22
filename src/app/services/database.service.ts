d736c233c87b2c875c29096a8a57cd4cad8deb17
Anthony DiGangi <digangi@Anthonys-Air.hsd1.ga.comcast.net>
2018-02-21 20:58:23 -0500

    updating database service and redirecting to https

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -7,23 +7,31 @@ import 'rxjs/add/operator/do';
  @Injectable()
  export class DatabaseService {
 
-   private API_URL = 'https://a6hghvxtg1.execute-api.us-east-1.amazonaws.com/dev/form/';
+   private API_URL = 'https://a6hghvxtg1.execute-api.us-east-1.amazonaws.com/qa/form/';
 
    constructor(private http: Http) { }
 
    createPerson(lastName, firstName, email, comments, option) {
      let headers = new Headers({
          'Content-Type' : 'application/json',
+         'X-Api-Key' : '1x44beiGUh12LUYFyfOOj2w8TqTACjXB1JZ9beTm',
         });
      let options = new RequestOptions({ headers: headers});
      let INFO =  Object.assign(lastName, firstName, email, comments, option);
      let body = JSON.stringify(INFO);
      console.log(options)
-     return this.http.post(this.API_URL, body, options).map((res: Response) => res.json());
+     return this.http.post(this.API_URL, body, options)
+        .map((res: Response) => res.json());
    }
 
    getInfo(id: string){
-     return this.http.get(this.API_URL + id).map((res:Response) => res.json());
+     let headers = new Headers({
+         'Content-Type' : 'application/json',
+         'X-Api-Key' : '1x44beiGUh12LUYFyfOOj2w8TqTACjXB1JZ9beTm',
+     });
+     let options = new RequestOptions({ headers: headers});
+     return this.http.get(this.API_URL + id, options)
+        .map((res:Response) => res.json());
    }
 
 }
